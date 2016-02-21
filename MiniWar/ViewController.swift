c2dd0db9432c1cd6a9e6efd38ecc8676131f034f
likedan <likedan5@icloud.com>
2016-02-21 09:39:00 -0600

    add button

--- MiniWar/ViewController.swift
+++ MiniWar/ViewController.swift
@@ -50,6 +50,13 @@ class ViewController: UIViewController {
         circle1.layer.shadowRadius = 3
         circle1.layer.shadowOpacity = 0.4
         
+        let lab1 = UILabel(frame: CGRect(x: 0, y: circle1.frame.origin.y + circleLength + 10, width: 200, height: 30))
+        lab1.center = CGPoint(x: circle1.center.x, y: lab1.center.y)
+        lab1.textColor = UIColor.grayColor()
+        lab1.alpha = 0.6
+        lab1.textAlignment = NSTextAlignment.Center
+        lab1.font = UIFont(name: "Avenir-Light", size: 22.0)
+        lab1.text = "I. Rural Land"
         
         circle2.backgroundColor = UIColor.whiteColor()
         circle2.frame = CGRect(x: self.view.frame.width * 1 / 8, y: self.view.frame.width * 1 / 8 + circleLength+200, width: circleLength, height: circleLength)
@@ -57,6 +64,13 @@ class ViewController: UIViewController {
         circle2.layer.shadowRadius = 3
         circle2.layer.shadowOpacity = 0.4
         
+        let lab2 = UILabel(frame: CGRect(x: 0, y: circle2.frame.origin.y + circleLength + 10, width: 200, height: 30))
+        lab2.center = CGPoint(x: circle1.center.x, y: lab2.center.y)
+        lab2.textColor = UIColor.grayColor()
+        lab2.alpha = 0.6
+        lab2.textAlignment = NSTextAlignment.Center
+        lab2.font = UIFont(name: "Avenir-Light", size: 22.0)
+        lab2.text = "II. Farm & Village"
         
         circle3.backgroundColor = UIColor.whiteColor()
         circle3.frame = CGRect(x: self.view.frame.width * 1 / 8, y: self.view.frame.width * 1 / 8 + 2 * circleLength + 400, width: circleLength, height: circleLength)
@@ -64,6 +78,13 @@ class ViewController: UIViewController {
         circle3.layer.shadowRadius = 3
         circle3.layer.shadowOpacity = 0.4
         
+        let lab3 = UILabel(frame: CGRect(x: 0, y: circle3.frame.origin.y + circleLength + 10, width: 200, height: 30))
+        lab3.center = CGPoint(x: circle1.center.x, y: lab3.center.y)
+        lab3.textColor = UIColor.grayColor()
+        lab3.alpha = 0.6
+        lab3.textAlignment = NSTextAlignment.Center
+        lab3.font = UIFont(name: "Avenir-Light", size: 22.0)
+        lab3.text = "III. War & Chao"
         
         entry.addSubview(rect1)
         entry.addSubview(rect2)
@@ -101,7 +122,10 @@ class ViewController: UIViewController {
         entry.addSubview(img1V)
         entry.addSubview(img2V)
         entry.addSubview(img3V)
-        
+        entry.addSubview(lab1)
+        entry.addSubview(lab2)
+        entry.addSubview(lab3)
+
         
         // UIColor(red: 239.0/255.0 , green: 239.0/255.0, blue: 244.0/255.0,
         
