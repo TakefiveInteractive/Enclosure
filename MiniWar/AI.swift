354200032f68d4840ebd203006ba8bdf70127821
likedan <likedan5@icloud.com>
2016-02-27 10:31:27 -0600

    trace path working

--- MiniWar/AI.swift
+++ MiniWar/AI.swift
@@ -11,50 +11,116 @@ import UIKit
 class AI: NSObject {
 
     let currentConfiguration: EnclosureGame
+    let rootBoard: AIBoard
     let lastSteps: [FenceNode]
     init(game: EnclosureGame, lastSteps: [FenceNode]) {
         currentConfiguration = game
         self.lastSteps = lastSteps
+        self.rootBoard = AIBoard(bigGame: currentConfiguration, lastMoves:  lastSteps)
         super.init()
     }
     
     func calculateNextStep(){
-        let start = NSDate()
-        let gg = AIBoard(bigGame: currentConfiguration)
-
-        var g = [AIBoard]()
-        for var x = 0; x < 1000; x++ {
-            g.append(AIBoard(copy: gg))
-        }
-        let end = NSDate();
-        let timeInterval: Double = end.timeIntervalSinceDate(start)
+        var possibleMove:Set<Set<NSArray>> = Set()
         
-        print("Time to evaluate problem : \(timeInterval) seconds");
+//        for var x = 0; x < rootBoard.boardSize; x++ {
+//            for var y = 0; y < rootBoard.boardSize; y++ {
+////                s.insert(Set([[x,y],[x,y+1]]))
+////                s.insert(<#T##member: Hashable##Hashable#>)
+////                if y + 3 < rootBoard.boardSize{
+////                    possibleMove.insert(Set([Set([[x,y],[x,y+1]]),Set([[x,1],[x,y+2]])]))
+////                }
+//            }
+//        }
+//        print(rootBoard.tempPath)
+        
+        print(rootBoard.getAllWays([0,0]).count)
+        print(rootBoard.getAllWays([2,0]).count)
+        print(rootBoard.getAllWays([2,0]).union(rootBoard.getAllWays([0,0])).count)
+
+//        for p in rootBoard.tempPath{
+//            print(p)
+//        }
+//        print(rootBoard.tempPath)
     }
-    
 }
 
 class AIBoard: NSObject {
     
+    
     let playerNum: Int
     let boardSize: Int
 
-    var nodes = [[(Int,Int)]]()
-    var fences = [((Int,Int),(Int,Int))]()
-    var lands = [[(Int,Int)]]()
+    var playerToGo: Int
+    
+    var nodes = [[[Int]]]() //[[(Int,Int)]]()
+    var fences = [Set<NSArray>]() //[((Int,Int),(Int,Int))]()
+    var lands = [[[Int]]]()
     
     var playerScore = [Int]()
     var playerFencesNum = [Int]()
     
-    var neutralLand = [(Int,Int)]()
-    var playerLand = [[(Int,Int)]]()
+    var neutralLand = [[Int]]()
+    var playerLand = [[[Int]]]()
     
-    var neutralFence = [((Int,Int),(Int,Int))]()
-    var playerFence = [[((Int,Int),(Int,Int))]]()
+    var neutralFence = [Set<NSArray>]()
+    var playerFence = [[Set<NSArray>]]()
     
-    init(copy: AIBoard) {
+    var selfLastMoves = [[Int]]()
+    var enemyLastMoves = [[Int]]()
+
+    // search all the connectable trace from a node
+    
+    func getAllWays(startPoint:[Int])->Set<Set<Set<NSArray>>>{
+        
+        var tempPath = Set<Set<Set<NSArray>>>()
+        
+        func getAllWaysBranch(head:[Int], path: Set<Set<NSArray>>){
+            if path.count < 2{//playerFencesNum[playerToGo]{
+                if head[0] - 1 >= 0{
+                    var tp = path
+                    if !tp.contains(Set([head,[head[0] - 1, head[1]]])){
+                        tp.insert(Set([head,[head[0] - 1, head[1]]]))
+                        getAllWaysBranch([head[0] - 1, head[1]], path: tp)
+                    }
+                }
+                if head[0] + 1 < boardSize{
+                    var tp = path
+                    if !tp.contains(Set([head,[head[0] + 1, head[1]]])){
+                        tp.insert(Set([head,[head[0] + 1, head[1]]]))
+                        getAllWaysBranch([head[0] + 1, head[1]], path: tp)
+                    }
+                }
+                if head[1] - 1 >= 0{
+                    var tp = path
+                    if !tp.contains(Set([head,[head[0], head[1] - 1]])){
+                        tp.insert(Set([head,[head[0], head[1] - 1]]))
+                        getAllWaysBranch([head[0], head[1] - 1], path: tp)
+                    }
+                }
+                if head[1] + 1 < boardSize{
+                    var tp = path
+                    if !tp.contains(Set([head,[head[0], head[1] + 1]])){
+                        tp.insert(Set([head,[head[0], head[1] + 1]]))
+                        getAllWaysBranch([head[0], head[1] + 1], path: tp)
+                    }
+                }
+            }else{
+                tempPath.insert(path)
+            }
+        }
+        
+        getAllWaysBranch(startPoint, path: Set<Set<NSArray>>())
+        
+        return tempPath
+        
+    }
+    
+    
+    init(var copy: AIBoard) {
         self.playerNum = copy.playerNum
         self.boardSize = copy.boardSize
+        self.playerToGo = (copy.playerToGo + 1) % 2
         super.init()
         
         self.nodes = copy.nodes
@@ -69,23 +135,34 @@ class AIBoard: NSObject {
         
         self.neutralFence = copy.neutralFence
         self.playerFence = copy.playerFence
+        
+        self.selfLastMoves = copy.selfLastMoves
+        self.enemyLastMoves = copy.enemyLastMoves
 
     }
     
-    init(bigGame: EnclosureGame) {
+    init(bigGame: EnclosureGame, lastMoves:[FenceNode]) {
         self.playerNum = bigGame.playerNum
         self.boardSize = bigGame.boardSize
+        self.playerToGo = bigGame.currentPlayer()
         super.init()
-        neutralLand = [(Int,Int)]()
-        playerLand = [[(Int,Int)]](count: playerNum, repeatedValue: [(Int,Int)]())
-        neutralFence = [((Int,Int),(Int,Int))]()
-        playerFence = [[((Int,Int),(Int,Int))]](count: playerNum, repeatedValue: [((Int,Int),(Int,Int))]())
+        
+        for node in lastMoves{
+            self.enemyLastMoves.append([node.x, node.y])
+        }
+        
+        neutralLand = [[Int]]()
+        playerLand = [[[Int]]](count: playerNum, repeatedValue: [[Int]]())
+        neutralFence = [Set<NSArray>]()
+        playerFence = [[Set<NSArray>]](count: playerNum, repeatedValue: [Set<NSArray>]())
+        playerFencesNum = bigGame.playerFencesNum
+        playerScore = bigGame.playerScore
         
         //create nodees
         for var x = 0; x < boardSize; x++ {
-            nodes.append([(Int,Int)]())
+            nodes.append([[Int]]())
             for var y = 0; y < boardSize; y++ {
-                nodes[x].append((x, y))
+                nodes[x].append([x, y])
             }
         }
         
@@ -93,21 +170,21 @@ class AIBoard: NSObject {
         for var x = 0; x <  boardSize; x++ {
             for var y = 0; y < boardSize; y++ {
                 if x < boardSize - 1{
-                    fences.append(((x,y),(x+1,y)))
+                    fences.append(Set([[x,y],[x+1,y]]))
                     
                     if bigGame.nodes[x][y].fences[bigGame.nodes[x+1][y]]!.player == -1{
-                        neutralFence.append(((x,y),(x+1,y)))
+                        neutralFence.append(Set([[x,y],[x+1,y]]))
                     }else{
-                        playerFence[bigGame.nodes[x][y].fences[bigGame.nodes[x+1][y]]!.player].append(((x,y),(x+1,y)))
+                        playerFence[bigGame.nodes[x][y].fences[bigGame.nodes[x+1][y]]!.player].append(Set([[x,y],[x+1,y]]))
                     }
                 }
                 if y < boardSize - 1{
-                    fences.append(((x,y),(x,y+1)))
+                    fences.append(Set([[x,y],[x,y+1]]))
                     
                     if bigGame.nodes[x][y].fences[bigGame.nodes[x][y+1]]!.player == -1{
-                        neutralFence.append(((x,y),(x,y+1)))
+                        neutralFence.append(Set([[x,y],[x,y+1]]))
                     }else{
-                        playerFence[bigGame.nodes[x][y].fences[bigGame.nodes[x][y+1]]!.player].append(((x,y),(x,y+1)))
+                        playerFence[bigGame.nodes[x][y].fences[bigGame.nodes[x][y+1]]!.player].append(Set([[x,y],[x,y+1]]))
                     }
                 }
             }
@@ -116,10 +193,10 @@ class AIBoard: NSObject {
         //create lands
         for var x = 0; x <  boardSize; x++ {
             if x < boardSize - 1{
-                lands.append([(Int,Int)]())
+                lands.append([[Int]]())
                 for var y = 0; y < boardSize; y++ {
                     if x < boardSize - 1 && y < boardSize - 1{
-                        let land = (x,y)
+                        let land = [x,y]
                         lands[x].append(land)
                         if bigGame.lands[x][y].player == -1{
                             neutralLand.append(land)
