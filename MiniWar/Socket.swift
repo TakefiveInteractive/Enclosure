66d27204a031346f3424c82bdda7d5fe7101937d
likedan <likedan5@icloud.com>
2016-03-02 20:36:21 -0600

    add socket

--- MiniWar/Socket.swift
+++ MiniWar/Socket.swift
@@ -3,22 +3,59 @@ import Foundation
 import SocketIOClientSwift
 
 
-public class Socket {
-    let socket = SocketIOClient(socketURL: NSURL(string: "http://o.hl0.co:3000")!, options: [.Log(true), .ForcePolling(true)])
+public class Socket: NSObject {
     
-    init() {
+    let socket:SocketIOClient
+    var isCreator = false
+    var roomNumber = "325879943"
+    
+    override init() {
+        socket = SocketIOClient(socketURL: NSURL(string: "http://o.hl0.co:3000")!, options: [.Log(false), .ForcePolling(true)])
+        super.init()
         self.addHandlers()
-        self.socket.connect()
+        socket.connect()
         print("init new sockect")
-        
+    }
+    
+    func createRoom() {
+        self.socket.emit("createRoom","")
+    }
+    
+    func searchRoom() {
+        self.socket.emit("joinRoom", roomNumber)
+    }
+    
+    func roomNotExist(){
+        // delegate
+        print("roomNotExist")
     }
     
     func addHandlers() {
         // Our socket handlers go here
         
+        //connected
+        self.socket.on("connect") { (data, ack) -> Void in
+            if self.isCreator{
+                self.createRoom()
+            }else{
+                self.searchRoom()
+            }
+        }
+        
+        self.socket.on("roomError") { (data, ack) -> Void in
+            
+        }
         // Using a shorthand parameter name for closures
+        self.socket.on("roomCreated") { (data, ack) -> Void in
+            print(data)
+        }
+        
+        self.socket.on("gameCanStart") { (data, ack) -> Void in
+            print(data)
+        }
+        
         self.socket.onAny {
-            print("Got event: \($0.event), with items: \($0.items)")
+            print("test \($0.event)  \($0.items)")
         }
         
         self.socket.on("connect") {data, ack in
@@ -36,12 +73,10 @@ public class Socket {
         }
         self.socket.on("gameReset") {data, ack in
             
-            
         }
     }
     
     func handleStart() {
-        
         
     }
     
@@ -49,10 +84,7 @@ public class Socket {
         
     }
     
-    func sendReady() {
-        self.socket.emit("ready","Ted")
-    }
-    
+
     func sendRestrat() {
         
         self.socket.emit("restart", [])
